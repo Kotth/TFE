2937e01e826b660fe7194f21b617f8c19a4c0dbc
Kotth <arnaud.nilens@gmail.com>
2021-02-28 19:30:10 +0100

    login and register components

---
+++ Spik/app/src/main/java/com/example/spik/RegisterActivity.kt
@@ -0,0 +1,72 @@
+package com.example.spik
+
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_register.*
+import java.util.*
+
+
+class RegisterActivity: AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        val adapter: ArrayAdapter<*> =
+            ArrayAdapter.createFromResource(this, R.array.languages_available, R.layout.spinner_item)
+        langSpinner.adapter = adapter
+
+        registerButton.setOnClickListener {
+            register()
+        }
+
+    }
+
+    private fun register() {
+
+            val email = emailAddressRegister.text.toString()
+            val password = passwordRegister.text.toString()
+
+            if(email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Email ou mot de passe incorrect", Toast.LENGTH_SHORT).show()
+                return
+            }
+
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if(!it.isSuccessful) return@addOnCompleteListener
+
+                    pushUserToFirebase()
+
+                    Toast.makeText(this, "Compte créé avec succès", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "Erreur lors de la création du compte: $it.message", Toast.LENGTH_SHORT).show()
+                }
+    }
+
+
+    private fun pushUserToFirebase() {
+        val uid = FirebaseAuth.getInstance().uid ?: ""
+        val database = FirebaseDatabase.getInstance("https://spik-app-default-rtdb.europe-west1.firebasedatabase.app/").getReference("/users/$uid")
+
+        val user = User(uid, pseudoRegister.text.toString(), langSpinner.selectedItem.toString())
+
+        database.setValue(user)
+    }
+}
+
+class User(val uid: String, val username: String, val lang: String)
