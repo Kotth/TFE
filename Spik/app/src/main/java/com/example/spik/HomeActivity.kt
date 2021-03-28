b4c6b4ded7c048f68ba6a43392d7ae4ca3fc7f56
Kotth <arnaud.nilens@gmail.com>
2021-03-28 21:02:29 +0200

    up

--- Spik/app/src/main/java/com/example/spik/HomeActivity.kt
+++ Spik/app/src/main/java/com/example/spik/HomeActivity.kt
@@ -1,11 +1,17 @@
 package com.example.spik
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
+
+    private val user = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -14,5 +20,31 @@ class HomeActivity : AppCompatActivity() {
         menuButton.setOnClickListener {
             menuDrawer.openDrawer(slider)
         }
+
+        slider.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.toString()) {
+                 "Modifier le profil" -> {
+                    Log.d("HomeActivity", "1")
+                    true
+                }
+                "Déconnexion" -> {
+                    user.signOut()
+                    val intent = Intent(this, LoginActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(intent)
+                    Toast.makeText(this, "Déconnecté avec succès", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                "Supprimer le compte" -> {
+                    Log.d("HomeActivity", "3")
+                    true
+                }
+                else -> {
+                    true
+                }
+            }
+        }
+
     }
+
 }
